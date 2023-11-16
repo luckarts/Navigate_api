d8d3ec9b11f9b0b72fa9b6af04d4871d5b2d30cf
luc <luc.bachelerieart@gmail.com>
2023-11-16 17:17:27 +0100

    feature ApiException valid test

--- app/Exceptions/ApiException.php
+++ app/Exceptions/ApiException.php
@@ -2,10 +2,34 @@
 
 namespace App\Exceptions;
 
-class ApiException {
+use App\Exceptions\ApiExceptionInterface;
 
-    public function throwError()
+
+class ApiException implements ApiExceptionInterface {
+
+    private $codeError;
+    private $message;
+
+    /**
+     * @param mixed $codeError : int
+     * @param mixed $message : sring
+     */
+    public function __construct(int $codeError,string $message){
+        $this->codeError = $codeError;
+        $this->message = $message;
+    }
+
+    /**
+     * *retourne une error json pour de l'api
+     * @return string
+     */
+    public function throwError(): string
     {
-
+        http_response_code($this->codeError);
+        $response = [
+            "error" => true,
+            "message" => $this->message
+        ];
+        return json_encode($response);
     }
 }
